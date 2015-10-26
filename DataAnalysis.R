f7121080acd8e38b8c9c4c05e14936e963c995ba
LarsMehwald <lars.mehwald@posteo.de>
2015-10-26 11:35:38 +0100

    Analysis

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -1,11 +1,13 @@
 ########################
 # Lars and Daniel 
-# 21 October 2015
-# Working with the PKS data frame 
+# 13 November 2015
+# Assignment 3
+# Loading PKS data and control variables
+# conducting statistical analyses 
 ########################
 
 # Setting the working directory
-setwd("D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to Collaborative Social Science Data Analysis/Assignment2")
+setwd("D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to Collaborative Social Science Data Analysis/Assignment3")
 
 # Loading required packages 
 library("rio")
@@ -18,61 +20,14 @@ pkgs <- c('dplyr', 'ggplot2', 'rio', 'tidyr', 'repmis')
 LoadandCite(pkgs, file = 'RpackageCitations.bib')
 rm(pkgs)
 
+########################
+# PKS data: Kreise 2013 2014
+########################
+
 # Scrapping the data
-# URL_PKS_faelle <- "http://www.bka.de/SharedDocs/Downloads/DE/Publikationen/PolizeilicheKriminalstatistik/2014/Zeitreihen/Faelle/tb01__FaelleGrundtabelleAb1987__csv,templateId=raw,property=publicationFile.csv/tb01__FaelleGrundtabelleAb1987__csv.csv"
-# URL_PKS_Kreise <- "http://www.bka.de/SharedDocs/Downloads/DE/Publikationen/PolizeilicheKriminalstatistik/2014/BKATabellen/FaelleLaenderKreiseStaedte/tb01__FaelleGrundtabelleKreise__csv,templateId=raw,property=publicationFile.csv/tb01__FaelleGrundtabelleKreise__csv.csv"
 URL_PKS_Kreise_13_14 <- "http://www.bka.de/SharedDocs/Downloads/DE/Publikationen/PolizeilicheKriminalstatistik/2014/BKATabellen/FaelleLaenderKreiseStaedte/tb01__FaelleGrundtabelleKreiseFallentwicklung__csv,templateId=raw,property=publicationFile.csv/tb01__FaelleGrundtabelleKreiseFallentwicklung__csv.csv"
-
-# PKS_faelle <- read.csv(URL_PKS_faelle, sep=";")
-# PKS_Kreise <- read.csv(URL_PKS_Kreise, sep=";")
 PKS_Kreise_13_14 <- read.csv(URL_PKS_Kreise_13_14, sep=";")
-
-# rm(URL_PKS_faelle)
-# rm(URL_PKS_Kreise)
 rm(URL_PKS_Kreise_13_14)
-
-########################
-# Kreise 2014
-########################
-
-# Viewing the Data
-View(PKS_Kreise)
-
-# Finding out the class of object
-class(PKS_Kreise)
-class(PKS_Kreise$X1)
-
-# Renaming variables 
-VarName1 <- PKS_Kreise[1,1] # Problem: also selection of level of variable 
-VarName1
-names(PKS_Kreise)[1] <- PKS_Kreise[1,1] # Problem persists
-
-# New apptempt to rename, this time manually 
-PKS_Kreise_13_14[1,]
-NameofVariables <- c("Schlüsselzahl der Tat", "Straftat", "Gemeindeschlüssel", "Stadt-/Landkreis", "erfasste Fälle", "HZ", "Versuche - Anzahl", "Versuche - Anteil in %", "mit Schusswaffe gedroht", "mit Schusswaffe geschossen", "aufgeklärte Fälle", "Aufklärungsquote", "Tatverdächtige insgesamt", "Tatverdächtige - männlich", "Tatverdächtige - weiblich", "Nichtdeutsche Tatverdächtige - Anzahl", "Nichtdeutsche Tatverdächtige - Anteil in %")
-names(PKS_Kreise) <- NameofVariables 
-rm(NameofVariables)
-
-# Delete the first row (before name of variable)
-PKS_Kreise <- PKS_Kreise[-c(1),]
-
-# Delete (for now) unimportant variables
-PKS_Kreise <- PKS_Kreise[,-c(1, 4, 6:17)]
-
-# Rearranging data
-PKS_Kreise <- PKS_Kreise[c(2,1,3)]
-
-# Reformatting data 
-
-# Do I need gather or spread? 
-# Do I need to reformat the data? 
-
-# Setting the class type to numeric: error
-# class(PKS_Kreise$"Gemeindeschlüssel") <- numeric # Why is this not working? 
-
-########################
-# Kreise 2013 2014
-########################
 
 # Delete (for now) unimportant variables
 PKS_Kreise_13_14 <- PKS_Kreise_13_14[,-c(1, 4, 7:15)]
@@ -101,8 +56,8 @@ rm(PKS_Kreise_13)
 rm(PKS_Kreise_14)
 
 # Removing variables (crimes) not relevant to analysis
-PKS_Kreise_13_spread <- PKS_Kreise_13_spread[,-c(3,4,5,6,11,12,13,14,15,16,17,18)]
-PKS_Kreise_14_spread <- PKS_Kreise_14_spread[,-c(3,4,5,6,11,12,13,14,15,16,17,18)]
+PKS_Kreise_13_spread <- PKS_Kreise_13_spread[,-c(3,4,5,6,11:18)]
+PKS_Kreise_14_spread <- PKS_Kreise_14_spread[,-c(3,4,5,6,11:18)]
 
 # Translation of variable names into English
 names(PKS_Kreise_13_spread)
@@ -117,13 +72,11 @@ PKS_Kreise_14_spread$year <- 2014
 
 # Combining district variable with year and rearranging
 PKS_Kreise_13_spread$district_year <- paste(PKS_Kreise_13_spread$district, "2013", sep = "_")
-PKS_Kreise_13 <- PKS_Kreise_13_spread[,-c(1)]
-PKS_Kreise_13 <- PKS_Kreise_13[,c(7,1,2,3,4,5,6)]
+PKS_Kreise_13 <- PKS_Kreise_13_spread[,c(8,1,2,3,4,5,6,7)]
 rm(PKS_Kreise_13_spread)
 
 PKS_Kreise_14_spread$district_year <- paste(PKS_Kreise_14_spread$district, "2014", sep = "_")
-PKS_Kreise_14 <- PKS_Kreise_14_spread[,-c(1)]
-PKS_Kreise_14 <- PKS_Kreise_14[,c(7,1,2,3,4,5,6)]
+PKS_Kreise_14 <- PKS_Kreise_14_spread[,c(8,1,2,3,4,5,6,7)]
 rm(PKS_Kreise_14_spread)
 
 # Combining the data frames 
@@ -138,66 +91,88 @@ PKS_Kreise[,4] <- as.numeric(as.character(PKS_Kreise[,4]))
 PKS_Kreise[,5] <- as.numeric(as.character(PKS_Kreise[,5]))
 PKS_Kreise[,6] <- as.numeric(as.character(PKS_Kreise[,6]))
 PKS_Kreise[,7] <- as.numeric(as.character(PKS_Kreise[,7]))
+PKS_Kreise[,8] <- as.numeric(as.character(PKS_Kreise[,8]))
 
 # Alternative changing of class - only for 2013
-PKS_Kreise_13_spread[,1] <- as.numeric(as.character(PKS_Kreise_13_spread[,1]))
-PKS_Kreise_13_spread[,2] <- as.numeric(as.character(PKS_Kreise_13_spread[,2]))
-PKS_Kreise_13_spread[,3] <- as.numeric(as.character(PKS_Kreise_13_spread[,3]))
-PKS_Kreise_13_spread[,4] <- as.numeric(as.character(PKS_Kreise_13_spread[,4]))
-PKS_Kreise_13_spread[,5] <- as.numeric(as.character(PKS_Kreise_13_spread[,5]))
-PKS_Kreise_13_spread[,6] <- as.numeric(as.character(PKS_Kreise_13_spread[,6]))
-PKS_Kreise_13_spread[,7] <- as.numeric(as.character(PKS_Kreise_13_spread[,7]))
+# PKS_Kreise_13_spread[,1] <- as.numeric(as.character(PKS_Kreise_13_spread[,1]))
+# PKS_Kreise_13_spread[,2] <- as.numeric(as.character(PKS_Kreise_13_spread[,2]))
+# PKS_Kreise_13_spread[,3] <- as.numeric(as.character(PKS_Kreise_13_spread[,3]))
+# PKS_Kreise_13_spread[,4] <- as.numeric(as.character(PKS_Kreise_13_spread[,4]))
+# PKS_Kreise_13_spread[,5] <- as.numeric(as.character(PKS_Kreise_13_spread[,5]))
+# PKS_Kreise_13_spread[,6] <- as.numeric(as.character(PKS_Kreise_13_spread[,6]))
+# PKS_Kreise_13_spread[,7] <- as.numeric(as.character(PKS_Kreise_13_spread[,7]))
 
 # Summarising data - commented out due to change in data frame 
+
+# Problem: treated (again) as factor variable
+
 title1 <- "These are the summary statistics for the year 2013"
 print(title1)
-# summary(PKS_Kreise_13_spread[,-c(1,7)])
+PKS_Kreise_13 <- PKS_Kreise[PKS_Kreise$year == 2013,]
+summary(PKS_Kreise_13[,c(3:7)])
 rm(title1)
+rm(PKS_Kreise_13)
 
 title2 <- "These are the summary statistics for the year 2014"
 print(title2)
-# summary(PKS_Kreise_14_spread[,-c(1,7)])
+PKS_Kreise_14 <- PKS_Kreise[PKS_Kreise$year == 2014,]
+summary(PKS_Kreise_14[,c(3:7)])
 rm(title2)
+rm(PKS_Kreise_14)
 
 # Saving the data 
-write.csv(PKS_Kreise_13_spread, file = "PKS_Kreise_13_spread.csv", append = "TRUE")
-write.csv(PKS_Kreise_14_spread, file = "PKS_Kreise_14_spread.csv", append = "TRUE")
+write.csv(PKS_Kreise, file = "data/PKS_Kreise.csv", append = "TRUE")
 
 ########################
 # German Elections 2013
 ########################
 
-setwd("C:/Users/larsunterwegs/Desktop/Crime")
-GermanElections <- read.csv(file = "GermanElections2013_modified.csv", sep=";", na.strings = "-")
-# Data manually downloaded and modified, Column names added and condensed
+# Downloading of data
+# Problem: unstable link
+# Data is badly formatted, hence modified manually using Excel
+# Column names added and condensed, some rows in the header deleted
+
+GermanElections <- read.csv(file = "data/GermanElections2013_ManuallyModified.csv", sep=";", na.strings = "-")
 
 # Removing previous elections
 GermanElection2013 <- GermanElections[c(1:525),]
+rm(GermanElections)
 
-# Why about 100 more observations than before? 
+# Why about 100 more observations than before in crime data frame? 
 
-# Removing Germany as a whole
+# Removing observation for Germany as a whole
 GermanElection2013 <- GermanElection2013[-1,]
 
 # Removing higher political units (they are coded with numbers below 1000)
 GermanElection2013$district <- as.numeric(as.character(GermanElection2013$Gemeindeschluessel))
 GermanElection2013 <- GermanElection2013[,c(13,4:12)]
-summary(GermanElection2013$district)
-class(GermanElection2013$district)
 
-GermanElection2013v2 <- GermanElection2013[GermanElection2013$district > 1000,]
-summary(GermanElection2013v2$district)
+summary(GermanElection2013$district)
+
+GermanElection2013 <- GermanElection2013[GermanElection2013$district > 1000,]
+summary(GermanElection2013$district)
 
 ########################
 # Merging Kreise 2013 with 
 # German Elections 2013
 ########################
 
-class (GermanElection2013v2)
-class(PKS_Kreise_13_spread)
-total <- merge(GermanElection2013v2, PKS_Kreise_13_spread, by="district")
+# Creating a PKS data frame only for 2013
 
-fit <- lm(robbery ~ Gültige.Zweitstimmen.CDU.CSU, data=total)
+# Problem with factors? 
+
+PKS_Kreise_13 <- PKS_Kreise[PKS_Kreise$year == 2013,]
+
+# Chechking whether both data frames are actually data frames
+class (GermanElection2013)
+class(PKS_Kreise_13)
+
+# Merging the data frames by the variable district
+# Districts that have no corresponding district are dropped
+ElectionCrime <- merge(GermanElection2013, PKS_Kreise_13, by="district")
+
+# Linear regression model 
+fit <- lm(robbery ~ Gültige.Zweitstimmen.CDU.CSU, data=ElectionCrime)
 summary(fit)
 
 rm(fit)
