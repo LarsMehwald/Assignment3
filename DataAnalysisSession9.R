81eecc3f04f73866a4ed1150aacdd7302a09779e
LarsMehwald <lars.mehwald@posteo.de>
2015-10-30 13:58:53 +0100

    Regression

--- DataAnalysisSession9.R
+++ DataAnalysisSession9.R
@@ -17,37 +17,42 @@ library("stargazer")
 library("texreg")
 library("lmtest")
 library("sandwich")
-library("Zelig")
+library("Zelig") # capital letter Z
 
 # Setting the commonly used working directory
-possible_dir <- c('D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to Collaborative Social Science Data Analysis/Assignment3', '~/HSoG/DataAnalysis/GitHub/Assignment3')
-rm(possible_dir)
+# possible_dir <- c('D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to Collaborative Social Science Data Analysis/Assignment3', '~/HSoG/DataAnalysis/GitHub/Assignment3')
+# rm(possible_dir)
 
 # Set to first valid directory in the possible_dir vector
-set_valid_wd(possible_dir)
+# set_valid_wd(possible_dir)
+setwd("D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to Collaborative Social Science Data Analysis/Assignment3")
 
 # Citing R packages 
-pkgs <- c('dplyr', 'ggplot2', 'rio', 'tidyr', 'repmis', 'httr')
-LoadandCite(pkgs, file = 'References/RpackageCitations.bib')
-rm(pkgs)
+# pkgs <- c('dplyr', 'ggplot2', 'rio', 'tidyr', 'repmis', 'httr')
+# LoadandCite(pkgs, file = 'References/RpackageCitations.bib')
+# rm(pkgs)
 
 # Loading merged data set for analysis
 MarriagesGraduatesCrimes2013 <- read.csv(file='data/MarriagesGraduatesCrimes2013.csv')
 
 # Linear regression model 
-names(MarriagesGraduatesCrimes2013)
+# names(MarriagesGraduatesCrimes2013)
 regrobbery <- lm(robbery ~ GraduatesWithHouthDegreeTotal + EhemannesTotal.EhefrauTotal, data=MarriagesGraduatesCrimes2013)
-summary(regrobbery)
+# summary(regrobbery)
 
-#After running regression
-regrobbery_hat <-fitted(regrobbery) #predicted values
-as.data.frame(regrobbery_hat)
-
-regrobbery_res <- residuals(regrobbery) #residuals 
-as.data.frame(regrobbery_res)
+# After running regression
+# regrobbery_hat <-fitted(regrobbery) #predicted values
+# as.data.frame(regrobbery_hat)
+# regrobbery_res <- residuals(regrobbery) #residuals 
+# as.data.frame(regrobbery_res)
 
 # Removing all estimations from lm
-rm(regrobbery)
+# rm(regrobbery)
+
+# Creating table output 
+stargazer(regrobbery, 
+          title = 'Logistic Regression Estimates of Grad School Acceptance',
+          digits = 2, type = 'html')
 
 # Removing everything from workspace
-rm(list=ls()) 
+# rm(list=ls()) 
