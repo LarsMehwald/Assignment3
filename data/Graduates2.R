94efcbee0f8f27af21c5b8fdde9e4947660914ca
dsalgadom <dsalgadom@hotmail.com>
2015-11-10 00:54:01 +0100

    Adding Graduates2 to merge and data folder

--- data/Graduates2.R
+++ data/Graduates2.R
@@ -38,4 +38,24 @@ Graduates2 <- read.csv(file="data/RawData/GraduatesProportionHochschulreifeHaupt
 # More compatibility with German characters
 Graduates2$DistrictName <- iconv(Graduates2$DistrictName, from ="latin1", to = "UTF-8")
 
-# Removing 
+# Removing DistrictName
+Graduates2 <- Graduates2[,-3]
+
+# Changing the class of some variables to numeric 
+Graduates2[,1] <- as.numeric(as.character(Graduates2[,1]))
+Graduates2[,2] <- as.numeric(as.character(Graduates2[,2]))
+Graduates2[,3] <- as.numeric(as.character(Graduates2[,3]))
+Graduates2[,4] <- as.numeric(as.character(Graduates2[,4]))
+
+# Removing higher political units, which are coded with numbers below 1000 (Länder) 
+# Hamburg and Berlin problematic: they have no further subunits 
+# Extract them first and then rbind them after all smaller units are removed 
+# 16 (02) is Hamburg; 364 is Berlin (11)
+# Attention: row numbers are not correctly counted as first row has been deleted 
+Graduates2HamburgBerlin <- Graduates2[c(16, 364),] 
+Graduates2 <- Graduates2[Graduates2$district > 1000,]
+Graduates2 <- rbind(Graduates2, Graduates2HamburgBerlin)
+rm(Graduates2HamburgBerlin)
+
+# Saving the data 
+write.csv(Graduates2, file = "data/Graduates2.csv")
