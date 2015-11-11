557e865a89a3b46f21f11d4b88ad7c50376f5c49
dsalgadom <dsalgadom@hotmail.com>
2015-11-11 20:13:05 +0100

    Added district ID to Foundations

--- data/Foundations2.R
+++ data/Foundations2.R
@@ -20,37 +20,36 @@ Foundations <- read.csv(file="data/RawData/Stiftungsdichte2013.csv",
                         dec=",",
                         na.strings=c("-", "."), 
                         header = FALSE,
-                        skip = 2,
+                        skip = 1,
                         nrows = 403, 
                         encoding = "UTF-8",
-                        col.names = c("Rank",
+                        col.names = c("district",
                                       "DistrictName",
                                       "State",
                                       "FoundationsDensity100k",
                                       "FoundationsTotal")
 )
 
-Foundations$DistrictName <- gsub(",","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Stadt","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Universitätsstadt","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Kreisfreie","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Hansestadt","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("krsfr.","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub(" Kreis","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Regionalverband","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Städteregion","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("^\\s+|\\s+$", "", Foundations$DistrictName) #Removes whitespaces before and after string
-Foundations$DistrictName <- gsub("(einschl. Aachen)", "", Foundations$DistrictName)
-Foundations$DistrictName <- gsub(" Landkreis","",Foundations$DistrictName)
-Foundations$DistrictName <- gsub("Soltau-Fallingbostel", "Landkreis HeideKreis", Foundations$DistrictName) #Change of name in 2011
-
+#Foundations$DistrictName <- gsub(",","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("Stadt","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("Universitätsstadt","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("Kreisfreie","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("Hansestadt","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("krsfr.","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub(" Kreis","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("Städteregion","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("^\\s+|\\s+$", "", Foundations$DistrictName) #Removes whitespaces before and after string
+#Foundations$DistrictName <- gsub("(einschl. Aachen)", "", Foundations$DistrictName)
+#Foundations$DistrictName <- gsub(" Landkreis","",Foundations$DistrictName)
+#Foundations$DistrictName <- gsub("Soltau-Fallingbostel", "Landkreis HeideKreis", Foundations$DistrictName) #Change of name in 2011
 
 # Changing the class of numbers
+Foundations[,1] <- as.numeric(as.character(Foundations[,1]))
 Foundations[,4] <- as.numeric(as.character(Foundations[,4]))
 Foundations[,5] <- as.numeric(as.character(Foundations[,5]))
 
 # Getting rid of redundant variables
-Foundations <- Foundations[,-c(1,3)]
+Foundations <- Foundations[,-c(2,3)]
 
 # Saving the data 
 write.csv(Foundations, file = "data/Foundations2.csv")
