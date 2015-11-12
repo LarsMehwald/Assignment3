f678e0a3efa6065293391b6edc8ab7f84667e5e4
dsalgadom <dsalgadom@hotmail.com>
2015-11-12 17:11:18 +0100

    New R file. Computation of additional variables
    I copied and pasted the variables you created in the Analysis files and
    made some important changes to rates, additions and new variables

---
+++ Analysis/CrimeSocEcoRates.R
@@ -0,0 +1,110 @@
+########################
+# Lars Mehwald and Daniel Salgado Moreno
+# 13 November 2015
+# Assignment 3
+# Additional variables (crime and socio economic rates)
+########################
+
+# Loading required packages 
+Packages <- c("rio", "dplyr", "tidyr", "repmis", "httr", "knitr", "ggplot2",
+              "xtable", "stargazer", "texreg", "lmtest", "sandwich", "Zelig",
+              "ggmap", "rworldmap")
+lapply(Packages, require, character.only = TRUE) 
+
+# Setting the commonly used working directory
+possible_dir <- c('D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to 
+                  Collaborative Social Science Data Analysis/Assignment3', 
+                  '~/HSoG/DataAnalysis/GitHub/Assignment3')
+set_valid_wd(possible_dir)
+rm(possible_dir)
+
+# Citing R packages 
+LoadandCite(Packages, file = 'References/RpackageCitations.bib')
+rm(Packages)
+
+# Loading data set from csv file
+DistrictData <- read.csv(file="Analysis/data/DistrictData2013.csv")
+
+# Removing ranking column (it was added in the saving process in DataMerging.R)
+DistrictData <- DistrictData[,-1]
+
+########################
+# Creation of relative measurements
+########################
+
+# Marraig rate: Number of marriages per 100,000 inhabitants 
+DistrictData$MarriageRate <- 
+  (DistrictData$HusbandAndWifeTotal / 
+  DistrictData$TotalPopulation) * 100000
+
+# Proportion of male population
+DistrictData$MaleRate <- 
+  (DistrictData$MalePopulation / 
+  DistrictData$TotalPopulation) * 100
+
+# Density per square km to 100 persons per square km ## Not necesassry 
+# DistrictData$DensityPerSQRTkm100 <- 
+  # DistrictData$DensityPerSQRTkm / 100
+
+# Relative conservative vote  
+### Not required, we can use TurnoutPercentage as a very solid measure of political participation
+#DistrictData$VoteConservativesPercent <- 
+ # DistrictData$VoteConservativesTotal * 100 / 
+  # (DistrictData$TurnoutPercentage * DistrictData$EntitledVoteTotal / 100)
+# TurnoutPercentage is coded in % between 0 and 100
+
+# Share of young individuals # I don't think we should include 0-17. Very low probability of child comiting crimes. 
+# Youth Rate is a better control variable
+DistrictData$YouthRate <- 
+  (DistrictData$Pop18to24 / 
+  DistrictData$TotalPopulation) * 100000
+
+# Share of Believers
+# Problematic: rates are significant lower in former East Germany 
+DistrictData$BelieversRate <- 
+  (DistrictData$BelieversTotal / 
+  DistrictData$TotalPopulation) * 100000
+
+# Net Mobility rate 
+DistrictData$FlowRate <- 
+  ((DistrictData$InfluxTotal -
+     DistrictData$OutflowTotal) /
+  DistrictData$TotalPopulation) * 100000
+
+########################
+# Create composite dependent variable:
+# (non-) violent and total crime 
+########################
+
+# violent crime
+DistrictData$CrimeViolent <- 
+  DistrictData$bodilyHarm + 
+  DistrictData$dangerousBodilyHarm +
+  DistrictData$violentCrime +
+  DistrictData$murderAndManslaughter +
+  DistrictData$robberyIncludingExtortionAndAttackOfCarDrivers
+
+# non-violent crime 
+DistrictData$CrimeNonViolent <- 
+  DistrictData$robberyFromOrOutOfCars + 
+  DistrictData$robberyOfCars +
+  DistrictData$vandalism +
+  DistrictData$vandalismGraffiti +
+  DistrictData$streetCrime +
+  DistrictData$burglaryDaylight +
+  DistrictData$burglary
+
+# total crimes
+DistrictData$CrimeTotal <- 
+  DistrictData$bodilyHarm + 
+  DistrictData$dangerousBodilyHarm +
+  DistrictData$violentCrime +
+  DistrictData$murderAndManslaughter +
+  DistrictData$robberyIncludingExtortionAndAttackOfCarDrivers +
+  DistrictData$robberyFromOrOutOfCars + 
+  DistrictData$robberyOfCars +
+  DistrictData$vandalism +
+  DistrictData$vandalismGraffiti +
+  DistrictData$streetCrime +
+  DistrictData$burglaryDaylight +
+  DistrictData$burglary
