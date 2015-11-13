9309127c5c19021a12ba959d1a1d37b54bf6b171
LarsMehwald <lars.mehwald@posteo.de>
2015-11-13 21:03:46 +0100

    Changing the models

--- Analysis/DataAnalysis.R
+++ Analysis/DataAnalysis.R
@@ -19,8 +19,7 @@ Packages <- c("rio", "dplyr", "tidyr", "repmis", "httr", "knitr", "ggplot2",
 lapply(Packages, require, character.only = TRUE) 
 
 # Setting the commonly used working directory
-possible_dir <- c('D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to 
-                  Collaborative Social Science Data Analysis/Assignment3', 
+possible_dir <- c('D:/Eigene Dokumente/!1 Vorlesungen/!! WS 2015/Introduction to Collaborative Social Science Data Analysis/Assignment3', 
                   '~/HSoG/DataAnalysis/GitHub/Assignment3')
 set_valid_wd(possible_dir)
 rm(possible_dir)
@@ -46,205 +45,142 @@ DistrictData <- DistrictData[,-1]
 # DistrictData <- DistrictData[-c(1,2),]
 
 # Linear regression model 1
-regression1 <- lm(CrimeViolentSum ~
-                    marriageRel +
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression1 <- lm(ViolentCrimeRate ~
+                    FoundationsDensity100k +
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
-                 data=DistrictData)
+                  data=DistrictData)
 summary(regression1)
 
 # Linear regression model 2
-regression2 <- lm(CrimeNonViolentSum ~ 
-                    marriageRel +
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression2 <- lm(NonViolentCrimeRate ~ 
+                    FoundationsDensity100k + 
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
-                 data=DistrictData)
+                  data=DistrictData)
 summary(regression2)
 
 # Linear regression model 3
-regression3 <- lm(CrimeTotalSum ~ 
-                    marriageRel +
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression3 <- lm(CrimeRate ~ 
+                    FoundationsDensity100k + 
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
                   data=DistrictData)
 summary(regression3)
 
 # Linear regression model 4
-regression4 <- lm(CrimeViolentSum ~ 
-                    BelieversPercent + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression4 <- lm(ViolentCrimeRate ~ 
+                    FlowRate + 
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
-                 data=DistrictData)
+                  data=DistrictData)
 summary(regression4)
 
 # Linear regression model 5
-regression5 <- lm(CrimeNonViolentSum ~ 
-                    BelieversPercent + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression5 <- lm(NonViolentCrimeRate ~ 
+                    FlowRate +
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
                   data=DistrictData)
 summary(regression5)
 
 # Linear regression model 6
-regression6 <- lm(CrimeTotalSum ~ 
-                    BelieversPercent + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression6 <- lm(CrimeRate ~ 
+                    FlowRate + 
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
                   data=DistrictData)
 summary(regression6)
 
 # Linear regression model 7
-regression7 <- lm(CrimeViolentSum ~ 
-                    FlowPercent +
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression7 <- lm(ViolentCrimeRate ~ 
+                    TurnoutPercentage + 
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
                   data=DistrictData)
 summary(regression7)
 
 # Linear regression model 8
-regression8 <- lm(CrimeNonViolentSum ~ 
-                    FlowPercent + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression8 <- lm(NonViolentCrimeRate ~ 
+                    TurnoutPercentage +
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
                   data=DistrictData)
 summary(regression8)
 
 # Linear regression model 9
-regression9 <- lm(CrimeTotalSum ~ 
-                    FlowPercent + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
+regression9 <- lm(CrimeRate ~ 
+                    TurnoutPercentage + 
+                    BelieversRate +
+                    MarriageRate +
+                    MaleRate +
+                    YouthRate +
                     UnemployedPercentage,
                   data=DistrictData)
 summary(regression9)
 
 # Linear regression model 10
-regression10 <- lm(CrimeViolentSum ~ 
-                    TurnoutPercentage +
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
-                    UnemployedPercentage,
-                  data=DistrictData)
+regression10 <- lm(ViolentCrimeRate ~
+                     FoundationsDensity100k +
+                     FlowRate +
+                     TurnoutPercentage +
+                     BelieversRate +
+                     MarriageRate +
+                     MaleRate +
+                     YouthRate +
+                     UnemployedPercentage,
+                   data=DistrictData)
 summary(regression10)
 
 # Linear regression model 11
-regression11 <- lm(CrimeNonViolentSum ~ 
-                    TurnoutPercentage + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
-                    UnemployedPercentage,
-                  data=DistrictData)
+regression11 <- lm(NonViolentCrimeRate ~ 
+                     FoundationsDensity100k +
+                     FlowRate +
+                     TurnoutPercentage +
+                     BelieversRate +
+                     MarriageRate +
+                     MaleRate +
+                     YouthRate +
+                     UnemployedPercentage,
+                   data=DistrictData)
 summary(regression11)
 
 # Linear regression model 12
-regression12 <- lm(CrimeTotalSum ~ 
-                    TurnoutPercentage + 
-                    DensityPerSQRTkm100 +
-                    PopulationYoung +
-                    MalePopulationRel +
-                    UnemployedPercentage,
-                  data=DistrictData)
+regression12 <- lm(CrimeRate ~ 
+                     FoundationsDensity100k +
+                     FlowRate +
+                     TurnoutPercentage +
+                     BelieversRate +
+                     MarriageRate +
+                     MaleRate +
+                     YouthRate +
+                     UnemployedPercentage,
+                   data=DistrictData)
 summary(regression12)
-
-# Linear regression model 13
-regression13 <- lm(CrimeViolentSum ~ 
-                     FoundationsTotal + 
-                     TurnoutPercentage +
-                     BelieversPercent +
-                     FlowPercent + 
-                     DensityPerSQRTkm100 +
-                     PopulationYoung +
-                     MalePopulationRel +
-                     UnemployedPercentage,
-                   data=DistrictData)
-summary(regression13)
-
-# Linear regression model 14
-regression14 <- lm(CrimeNonViolentSum ~ 
-                     FoundationsTotal + 
-                     TurnoutPercentage +
-                     BelieversPercent +
-                     FlowPercent + 
-                     DensityPerSQRTkm100 +
-                     PopulationYoung +
-                     MalePopulationRel +
-                     UnemployedPercentage,
-                   data=DistrictData)
-summary(regression14)
-
-# Linear regression model 15
-regression15 <- lm(CrimeTotalSum ~ 
-                     FoundationsTotal + 
-                     TurnoutPercentage +
-                     BelieversPercent +
-                     FlowPercent + 
-                     DensityPerSQRTkm100 +
-                     PopulationYoung +
-                     MalePopulationRel +
-                     UnemployedPercentage,
-                   data=DistrictData)
-summary(regression15)
-
-# Linear regression model 16
-regression16 <- lm(CrimeViolentSum ~ 
-                     marriageRel + 
-                     TurnoutPercentage +
-                     BelieversPercent +
-                     FlowPercent +
-                     FoundationsTotal +
-                     DensityPerSQRTkm100 +
-                     PopulationYoung +
-                     MalePopulationRel +
-                     UnemployedPercentage,
-                   data=DistrictData)
-summary(regression16)
-
-# Linear regression model 17
-regression17 <- lm(CrimeNonViolentSum ~ 
-                     marriageRel + 
-                     TurnoutPercentage +
-                     BelieversPercent +
-                     FlowPercent + 
-                     FoundationsTotal + 
-                     DensityPerSQRTkm100 +
-                     PopulationYoung +
-                     MalePopulationRel +
-                     UnemployedPercentage,
-                   data=DistrictData)
-summary(regression17)
-
-# Linear regression model 18
-regression18 <- lm(CrimeTotalSum ~ 
-                     marriageRel + 
-                     TurnoutPercentage +
-                     BelieversPercent +
-                     FlowPercent + 
-                     FoundationsTotal + 
-                     DensityPerSQRTkm100 +
-                     PopulationYoung +
-                     MalePopulationRel +
-                     UnemployedPercentage,
-                   data=DistrictData)
-summary(regression18)
 
 # After running regression
 # regrobbery_hat <- fitted(regrobbery) #predicted values
@@ -304,8 +240,23 @@ z3 <- glm.nb(CrimeRate ~
                UnemployedPercentage,
              subset1)
 
-# Creating table output 
-stargazer(regression1, regression2, regression3, 
+# negative Binomial regression model 4
+z4 <- glm.nb(CrimeRate ~ 
+               FoundationsDensity100k +
+               FlowRate +
+               TurnoutPercentage +
+               BelieversRate +
+               MarriageRate +
+               MaleRate +
+               YouthRate +
+               UnemployedPercentage,
+             subset1)
+
+########################
+# Creating table output
+########################
+ 
+stargazer(regression1, regression2, regression3, z1,
           type = "latex",
           header = FALSE, # important not to have stargazer information in markdown file 
           title = "Regression analysis regarding (non-) violent 
@@ -315,7 +266,7 @@ stargazer(regression1, regression2, regression3,
           notes = "This regression output shows the results using 3 different 
           dependent variables")
 
-stargazer(regression4, regression5, regression6, 
+stargazer(regression4, regression5, regression6, z2,
           type = "latex",
           header = FALSE, # important not to have stargazer information in markdown file 
           title = "Regression analysis regarding (non-) violent and total crimes 
@@ -325,7 +276,7 @@ stargazer(regression4, regression5, regression6,
           notes = "This regression output shows the results using 3 different 
           dependent variables")
 
-stargazer(regression7, regression8, regression9, 
+stargazer(regression7, regression8, regression9, z3,
           type = "latex",
           header = FALSE, # important not to have stargazer information in markdown file 
           title = "Regression analysis regarding (non-) violent and total crimes 
@@ -335,7 +286,7 @@ stargazer(regression7, regression8, regression9,
           notes = "This regression output shows the results using 3 different 
           dependent variables")
 
-stargazer(regression10, regression11, regression12, 
+stargazer(regression10, regression11, regression12, z4,
           type = "latex",
           header = FALSE, # important not to have stargazer information in markdown file 
           title = "Regression analysis regarding (non-) violent and total crimes 
@@ -345,28 +296,7 @@ stargazer(regression10, regression11, regression12,
           notes = "This regression output shows the results using 3 different 
           dependent variables")
 
-stargazer(regression13, regression14, regression15, 
-          type = "latex",
-          header = FALSE, # important not to have stargazer information in markdown file 
-          title = "Regression analysis regarding (non-) violent and total crimes 
-          and independent variable foundation",
-          digits = 2,
-          omit.stat = c("f", "ser"),
-          notes = "This regression output shows the results using 3 different 
-          dependent variables")
-
-stargazer(regression16, regression17, regression18, 
-          type = "latex",
-          header = FALSE, # important not to have stargazer information in markdown file 
-          title = "Regression analysis regarding (non-) violent and total crimes 
-          and all independent variables at the same time",
-          digits = 2,
-          omit.stat = c("f", "ser"),
-          notes = "This regression output shows the results using 3 different 
-          dependent variables")
-
 # Removing regression results 
 rm(regression1, regression2, regression3, regression4, regression5, regression6, 
-   regression7, regression8, regression9, regression10, regression11, regression12,
-   regression13, regression14, regression15, regression16, regression17, regression18)
-
+   regression7, regression8, regression9, regression10, regression11, regression12)
+rm(z1, z2, z3, z4)
