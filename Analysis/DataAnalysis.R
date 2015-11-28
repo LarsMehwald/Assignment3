6e660084c4ace51e4a3fc9c4fd458b08453123eb
LarsMehwald <lars.mehwald@posteo.de>
2015-11-28 18:22:07 +0100

    Small changes

--- Analysis/DataAnalysis.R
+++ Analysis/DataAnalysis.R
@@ -15,7 +15,7 @@
 # Loading required packages 
 Packages <- c("rio", "dplyr", "tidyr", "repmis", "httr", "knitr", "ggplot2",
               "xtable", "stargazer", "texreg", "lmtest", "sandwich", "Zelig",
-              "ggmap", "rworldmap","car", "MASS", "PerformanceAnalytics", "pscl", "AER")
+              "car", "MASS", "PerformanceAnalytics", "pscl", "AER")
 lapply(Packages, require, character.only = TRUE) 
 
 # Setting the commonly used working directory
