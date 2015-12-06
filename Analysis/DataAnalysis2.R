91dfb1e91bb1f301fb09354c86d39ea80e4736ed
LarsMehwald <lars.mehwald@posteo.de>
2015-12-06 14:52:23 +0100

    Commenting on incident rates and statistical significance

--- Analysis/DataAnalysis2.R
+++ Analysis/DataAnalysis2.R
@@ -192,23 +192,36 @@ nb.glm1 <- glm.nb(Murder ~
                     offset(log(TotalPopulation)), # Offset tunrs counts into per capita rates
                   DistrictData)
 
+# Incident rates and statistical significance
 # Adding coefficients and confident intervals into new data frame 
+
+# Extract coefficients and create confidence intervals for three levels of significance
 est1 <- cbind(Estimate = coef(nb.glm1), 
               confint(nb.glm1, level=0.90),
               confint(nb.glm1, level=0.95),
               confint(nb.glm1, level=0.99))
 est1 <- data.frame(est1)
+
+# Adding three variables (1=TRUE) describing whether coefficient is significant at 
+# a given level of statistical significance 
 est1 <- cbind(est1, 
               ifelse(sign(est1[2]) == sign(est1[3]), 1, 0),
               ifelse(sign(est1[4]) == sign(est1[5]), 1, 0),
               ifelse(sign(est1[6]) == sign(est1[7]), 1, 0))
 est1 <- round(est1, 4)
+
+# Creating a new variable that indicates the level of statistical significance
+# 0=not significant, 1=at 90%level, 2=at 95% level, 3=at 99%level
 est1 <- cbind(est1, 
               ifelse(est1[10] == 1, 3, 
               ifelse(est1[9] == 1, 2,
               ifelse(est1[8] == 1, 1, 0))))
 est1 <- est1[,-c(2:10)]
+
+# Creating indicrnt rates by exponentiating the coefficients
 est1 <- cbind(exp(est1[1]), est1[2])
+
+# Creating new variable with stars * 
 names(est1) <- c("Coefficient", "NumberStars")
 est1 <- round(est1, 4)
 est1$NumberStars <- as.factor(est1$NumberStars)
